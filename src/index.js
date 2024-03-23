382dd52b7e471c3d5efa528480ccdc884a54f04a
Nataliia Melnyk <natalia.melnyk28@gmail.com>
2024-03-23 17:40:08 +0200

    Added redux Persist

--- src/index.js
+++ src/index.js
@@ -2,17 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
 import { BrowserRouter } from "react-router-dom";
 import { App } from '../src/components/App';
 import './index.css';
-// import dotenv from 'dotenv';
-// dotenv.config();
+import { persistor} from "./redux/store";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
     < BrowserRouter>
+    <PersistGate loading={null} persistor={persistor}>
     <App />
+    </PersistGate>
     </BrowserRouter>
     </Provider>
   </React.StrictMode>
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,30 @@
 import { configureStore } from "@reduxjs/toolkit";
-
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage'
 import {teachersReducer} from "./teachers/teachers-slice";
-
+const persisTeacherstConfig = {
+  key: 'teachers',
+  storage,
+}
+const persistedTeachersReducer=persistReducer(persisTeacherstConfig,teachersReducer)
  export const store = configureStore({
   reducer:{
-    teachers:teachersReducer,
+    teachers:persistedTeachersReducer,
   },
+  middleware: getDefaultMiddleware =>
+  getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
 });
+export const persistor = persistStore(store);
