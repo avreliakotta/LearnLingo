0b42c8b8a6ccff37df5a151a6d8bdb3d1698afac
Nataliia Melnyk <natalia.melnyk28@gmail.com>
2024-03-26 14:06:27 +0200

    Change name  in package.json

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
 import { PersistGate } from 'redux-persist/integration/react';
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { App } from '../src/components/App';
 import './index.css';
 import { persistor} from "./redux/store";
@@ -11,11 +11,11 @@ import { persistor} from "./redux/store";
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-    < HashRouter>
+    < BrowserRouter>
     <PersistGate loading={null} persistor={persistor}>
     <App />
     </PersistGate>
-    </HashRouter>
+    </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
