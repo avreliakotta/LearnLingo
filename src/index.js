288e11c62aa486acfc4f0743555aaf3cff6d3a08
Nataliia Melnyk <natalia.melnyk28@gmail.com>
2024-03-26 14:14:52 +0200

    Changed DrowserRoute to HashRoute

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
 import { PersistGate } from 'redux-persist/integration/react';
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { App } from '../src/components/App';
 import './index.css';
 import { persistor} from "./redux/store";
@@ -11,11 +11,11 @@ import { persistor} from "./redux/store";
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-    < BrowserRouter>
+    < HashRouter>
     <PersistGate loading={null} persistor={persistor}>
     <App />
     </PersistGate>
-    </BrowserRouter>
+    </HashRouter>
     </Provider>
   </React.StrictMode>
 );
