7c72a0af31985a1578984f8bd9c6e87e85c3d798
Nataliia Melnyk <natalia.melnyk28@gmail.com>
2024-03-31 22:53:58 +0300

    Fdded logic button "Get started"

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,15 +1,20 @@
-
+import { useNavigate } from 'react-router';
 import css from "./home-page.module.css";
 import image from "../../img/image.jpg";
 import InfoBlock from "../../components/InfoBlock/InfoBlock";
 const HomePage=()=>{
+    const navigate=useNavigate();
+    const handlerClick=()=>{
+       navigate("/teachers")  
+    }
     return(
+        
         <main>
             <section className={css.heroWrapper}>
                 <div className={css.contentWrapper}>
                     <h1 className={css.heroTitle}>Unlock your potential with the best <span className={css.part}>language</span> tutors</h1>
                     <p className={css.heroText}>Embark on an Exciting Language Journey with Expert Language Tutors: Elevate your language proficiency to new heights by connecting with highly qualified and experienced tutors.</p>
-                    <button type="button" className={css.heroBtn}>Get started</button>
+                    <button type="button" className={css.heroBtn} onClick={handlerClick}>Get started</button>
                 </div>
                 <div className={css.imgThumb}>
                 <img src={image} alt="girl with leptop" className={css.heroImg}/>
