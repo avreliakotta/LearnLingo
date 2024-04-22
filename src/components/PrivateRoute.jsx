ae92f6f40c5f6a129905706420f18357299ba530
Nataliia Melnyk <natalia.melnyk28@gmail.com>
2024-04-22 12:08:21 +0300

    Remove console logs

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,11 +1,10 @@
 import { Navigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-import { selectIsAuth} from '../redux/auth/auth-selectors';
+import { selectIsAuth } from '../redux/auth/auth-selectors';
 import FavoritesPage from '../pages/FavoritesPage/FavoritesPage';
 const PrivateRoute = () => {
   const isLogin = useSelector(selectIsAuth);
-  console.log("isLogin",isLogin);
-  
-  return isLogin ? <FavoritesPage/> : <Navigate to="/" replace  />;
+
+  return isLogin ? <FavoritesPage /> : <Navigate to="/" replace />;
 };
 export default PrivateRoute;
