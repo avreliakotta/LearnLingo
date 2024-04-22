a1d56ab2adc97a10c4869d005c71d3a0b7e2a7d2
Nataliia Melnyk <natalia.melnyk28@gmail.com>
2024-04-22 09:36:08 +0300

    Update firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,17 +1,5 @@
 import { initializeApp } from 'firebase/app';
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyC6RHNbyV2ppnLICbblb9t3Rsz-h-CMhNg",
-//   authDomain: "learnlingo-8b086.firebaseapp.com",
-//   databaseURL: "https://learnlingo-8b086-default-rtdb.europe-west1.firebasedatabase.app",
-//   projectId: "learnlingo-8b086",
-//   storageBucket: "learnlingo-8b086.appspot.com",
-//   messagingSenderId: "335547383683",
-//   appId: "1:335547383683:web:44df5719e29fce549400b6",
-//   measurementId: "G-73VRE4JXE3"
-// };
-console.log('API Key:', process.env.REACT_APP_FIREBASE_API_KEY);
-console.log('Auth Domain:', process.env.REACT_APP_FIREBASE_AUTH_DOMAIN);
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
@@ -22,7 +10,7 @@ const firebaseConfig = {
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
   measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
-}
+};
 console.log('Firebase Config:', firebaseConfig);
 
 const app = initializeApp(firebaseConfig);
